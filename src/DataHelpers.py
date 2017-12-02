bf615927015641e7925f17b31866beef7a9f5f05
chfe910 <chfe910@gmail.com>
2017-12-02 21:07:38 +0800

    add shuffle_data

--- src/DataHelpers.py
+++ src/DataHelpers.py
@@ -1,1 +1,9 @@
 # -*- coding: utf-8 -*-
+
+
+import numpy as np
+
+
+def shuffle_data(list_X, list_Y):
+    shuffle_indices = np.random.permutation(np.arange(len(list_X)))
+    return np.array(list_X)[shuffle_indices], np.array(list_Y)[shuffle_indices]
